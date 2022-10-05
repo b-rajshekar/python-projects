72f77f64bf6fd162a197adffc5169a37a6bd0506
b-rajshekar <91706770+b-rajshekar@users.noreply.github.com>
2022-10-05 10:41:41 +0530

    Update emailslicer.py

--- emailslicer.py
+++ emailslicer.py
@@ -1,6 +1,6 @@
 # collect email from user
 # split the email using the @, the first part as the user name, the second part is gonna be saved as domain
-# split domain using.,
+# split domain using .
 
 import email
 
