8368cf86100f953b774aa99136adbd423ede80ee
Bujjagowns Rajashekhar <91706770+b-rajshekar@users.noreply.github.com>
2023-07-25 07:05:12 +0530

    initial commit
    Added location, city and current time to the weatherApp.

--- weatherApp.py
+++ weatherApp.py
@@ -1,1 +1,22 @@
+import requests
+import json
+import pyttsx3
 
+
+def speak(message):
+    engine = pyttsx3.init()
+    engine.say(message)
+    engine.runAndWait()
+
+
+city = input("Enter the name of the city\n")
+url = f"https://api.weatherapi.com/v1/current.json?key=036beaa8954b4d2196d04644232507&q={city}"
+
+r = requests.get(url)
+weatherDIC = json.loads(r.text)
+ltimeDIC = json.loads(r.text)
+
+w = weatherDIC["current"]["temp_c"]
+lt = ltimeDIC["location"]["localtime"]
+
+speak(f" 'The current weather in {city} is {w} degrees as of {lt}'")
